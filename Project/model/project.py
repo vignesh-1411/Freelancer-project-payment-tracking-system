0dc9383acf6c8fd2fe7c1fb6a975ca680ae89358
neojarvis <techadmin@iamneo.ai>
2026-05-13 17:26:18 +0000

    Auto-save-commit-13/05/2026--22:56:14

--- Project/model/project.py
+++ Project/model/project.py
@@ -1,2 +1,13 @@
 class Project:
-    def __init__(self, project_id=None,client_id=None,freelancer_id=None,project_name=None,description=None,deadline=None,status="OPEN")
+    def __init__(self, project_id=None,client_id=None,freelancer_id=None,project_name=None,description=None,deadline=None,status="OPEN"):
+        self.project_id=project_id
+        self.client_id=client_id
+        self.freelancer_id=freelancer_id
+        self.project_name=project_name
+        self.description=description
+        self.deadline=deadline
+        self.status=status
+
+    def __str__(self):
+        return f"{self.project_id}, {self.client_id}, {self.freelancer_id}, {self.project_name}, {self.description}, {self.deadline}, {self.status}"
+        
