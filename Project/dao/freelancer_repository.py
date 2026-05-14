715e324f2b5b4a7b8cf6303c1c22d98fddfdadee
neojarvis <techadmin@iamneo.ai>
2026-05-14 17:50:53 +0000

    Auto-save-commit-14/05/2026--23:20:52

--- Project/dao/freelancer_repository.py
+++ Project/dao/freelancer_repository.py
@@ -7,4 +7,36 @@ from model.project import Project
 from model.task import Task
 
 class FreelancerRepository(ABC):
-    pass 
+    
+    @abstractmethod
+    def add_freelancer(self,freelancer:Freelancer) -> bool:
+        pass
+    
+    @abstractmethod
+    def update_freelancer(self,freelancer:Freelancer) -> bool:
+        pass
+    
+    @abstractmethod
+    def delete_freelancer(self,freelancer_id:int) -> bool:
+        pass
+
+    @abstractmethod
+    def get_freelancer_by_id(self,freelancer_id:int) -> Freelancer:
+        pass
+
+    @abstractmethod
+    def add_client(self,client:Client) -> bool:
+        pass
+    
+    @abstractmethod
+    def update_client(self,client:Client) -> bool:
+        pass
+    
+    @abstractmethod
+    def delete_client(self,client_id:int) -> bool:
+        pass
+
+    @abstractmethod
+    def get_freelancer_by_id(self,freelancer_id:int) -> Freelancer:
+        pass
+    
