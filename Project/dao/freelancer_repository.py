cb0337459d4e2407e5a5b45d3618046ace5ecf90
neojarvis <techadmin@iamneo.ai>
2026-05-14 17:30:58 +0000

    Manual-save-commit-14/05/2026--23:00:57

---
+++ Project/dao/freelancer_repository.py
@@ -0,0 +1,10 @@
+from abc import ABC,abstractmethod
+
+from model.client import Client
+from model.freelancer import Freelancer
+from model.payment import Payment
+from model.project import Project
+from model.task import Task
+
+class FreelancerRepository(ABC):
+    pass 
---
+++ Project/dao/freelancer_repository_impl.py
@@ -0,0 +1,1 @@
+from dao.freelancer_repository import FreelancerRepository
