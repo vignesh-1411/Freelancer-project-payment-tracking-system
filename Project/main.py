b03bc6fb6853800d40bb178171ff1c5d315022ac
neojarvis <techadmin@iamneo.ai>
2026-05-15 11:07:17 +0000

    Submit-project-save-commit-15/05/2026--16:37:15

--- Project/main.py
+++ Project/main.py
@@ -5,11 +5,11 @@ from dao.freelancer_repository_impl import FreelancerRepositoryImpl
 class FreelancerApp:
 
     def __init__(self):
-        # self.repo = FreelancerRepositoryImpl()
+        self.repo = FreelancerRepositoryImpl()
 
 #Write your code here
 
 if __name__ == '__main__':
     setup_database()
-    app = FreelancerApp()
-    app.run()
+    # app = FreelancerApp()
+    # app.run()
