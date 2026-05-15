380bfbf4d057df7b4a550ba05883c2ea034cac05
neojarvis <techadmin@iamneo.ai>
2026-05-15 07:23:03 +0000

    Submit-project-save-commit-15/05/2026--12:52:58

--- Project/dao/freelancer_repository_impl.py
+++ Project/dao/freelancer_repository_impl.py
@@ -1,3 +1,19 @@
 from dao.freelancer_repository import FreelancerRepository
+from model.freelancer import Freelancer
+from model.client import Client
+from model.payment import Payment
+from model.project import Project
+from model.task import Task
+from util.db_connection import DBConnection 
+from exception.client_not_found_exception import ClientNotFoundException 
+from exception.freelancer_not_found_exception import FreelancerNotFoundException
+from exception.invalid_payment_exception import InvalidPaymentException
+from exception.project_closure_exception  import ProjectClosureException
+
+
 class FreelancerRepositoryImpl(FreelancerRepository):
-    pass
+    def __init__(self):
+        self.conn=DBConnection.get_connection()
+        self.cursor=self.conn.cursor()
+
+    
