f687a4f9407b50a50dee964406c110f8ee7fcd61
neojarvis <techadmin@iamneo.ai>
2026-05-15 11:04:07 +0000

    Auto-save-commit-15/05/2026--16:34:06

--- Project/main.py
+++ Project/main.py
@@ -1,8 +1,11 @@
 from util.db_setup import setup_database
+from dao.freelancer_repository import FreelancerRepository
+from dao.freelancer_repository_impl import FreelancerRepositoryImpl
+
 class FreelancerApp:
 
     def __init__(self):
-        self.repo = FreelancerRepositoryImpl()
+        # self.repo = FreelancerRepositoryImpl()
 
 #Write your code here
 
