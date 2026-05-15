d2ea1379961d535e5482c209412a7d13bbc43392
neojarvis <techadmin@iamneo.ai>
2026-05-15 11:45:56 +0000

    Manual-save-commit-15/05/2026--17:15:55

--- Project/main.py
+++ Project/main.py
@@ -18,6 +18,35 @@ class FreelancerApp:
 
 #Write your code here
 
+    def display_menu(self):
+        print("-----------------------------FREELANCER PROJECT AND PAYMENT TRACKING SYSTEM----------------------------------")
+        print("Freelancer Management------")
+        print("1. Add Freelancer")
+        print("2. Update Freelancer")
+        print("3. Delete Freelancer")
+        print("4. View Freelancer by ID")
+        print("Client Management--------")
+        print("5. Add Client")
+        print("6. Update Client")
+        print("7. Delete Client")
+        print("Project Management---------")
+        print("8. Create Project")
+        print("9. Update Project status")
+        print("10. View project by freelancer")
+        print("11. View project by client")
+        print("Task Management-------------")
+        print("12. Add Task")
+        print("13. Update Task status")
+        print("14. View tasks by project")
+        print("Payment Management-------------")
+        print("15. Process Payment")
+        print("16. View payments by project")
+        print("System----------")
+        print("17. View all payments")
+        print("18. EXIT")
+
+    
+
 if __name__ == '__main__':
     setup_database()
     # app = FreelancerApp()
